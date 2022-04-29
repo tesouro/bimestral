bd17462d9e32e3a1ea278d226fe12d4d4f07afc8
tiagombp <tiagombp@gmail.com>
2022-04-29 16:14:07 -0300

    Update prep.R

--- data-prep/prep.R
+++ data-prep/prep.R
@@ -152,6 +152,7 @@ desp_det_pre <- desp_det %>%
     reav = !!sym(termo_reav))
     
 desp_det_pre$percent_reav <- desp_det_pre$reav / sum(desp_det_pre$reav)
+
 desp_det_export <- desp_det_pre %>%
   arrange(-valor_quadradinhos_reav) %>%
   mutate(
